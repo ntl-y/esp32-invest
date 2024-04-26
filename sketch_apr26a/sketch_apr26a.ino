0b4ce18b840ac6ae68a1686cfbe22d8fad6674db
ntl-y <n-danilkina@list.ru>
2024-04-26 15:49:34 +0300

    serialization

--- sketch_apr26a/sketch_apr26a.ino
+++ sketch_apr26a/sketch_apr26a.ino
@@ -2,7 +2,7 @@
 #include <U8g2lib.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
-
+#include <ArduinoJson.h>
 
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
@@ -14,7 +14,8 @@
 const char* ssid = "Honor 9";
 const char* password = "qwerty1234";
 
-String serverName = "http://192.168.1.1:8999/";
+int exchange_rate = 0;
+String serverName = "http://31.129.104.112:8999/";
 
 U8G2_ST7565_ZOLEN_128X64_F_4W_SW_SPI u8g2(U8G2_R0,/* clock=*/ 27, /* data=*/ 26, /* cs=*/ 13, /* dc=*/ 14, /* reset=*/ 12);
 
@@ -29,38 +30,44 @@ void setup(void) {
   Serial.println("Подключено к Wi-Fi!");
   Serial.println(WiFi.localIP());
 
-  HTTPClient http;
-  http.begin(serverName);
-
-    // Отправляем GET-запрос
-  int httpCode = http.GET();
-
-  if (httpCode > 0) {
-    // Если ответ успешный
-    if (httpCode == HTTP_CODE_OK) {
-      String payload = http.getString();
-      Serial.println(payload); // Выводим ответ на Serial Monitor
-    }
-  } else {
-    Serial.println("Ошибка при отправке GET-запроса!");
-  }
-
-  http.end(); // Завершаем сеанс HTTP
-
   u8g2.begin();
   u8g2.sendF("ca", 0xa0, 0x41);
 }
 
 void loop(void) {
+    HTTPClient http;
+  http.begin(serverName);
+
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload); 
+
+      DynamicJsonDocument doc(1024); 
+      DeserializationError error = deserializeJson(doc, payload);
+
+      if (error) {
+        Serial.print("Ошибка при распарсинге JSON: ");
+        Serial.println(error.c_str());
+      } else {
+        exchange_rate = doc["random_number"];
+      }
+    }
+  } else {
+    Serial.println("Ошибка при отправке GET-запроса!");
+  }
+
+  http.end(); 
+
   u8g2.clearBuffer();				
   u8g2.enableUTF8Print(); 
   u8g2.setFont(u8g2_font_6x13B_t_cyrillic);	
   u8g2.setCursor(4, 22);
   u8g2.setContrast (80);
-   u8g2.print("Привет");
-    u8g2.setFont(u8g2_font_cu12_t_cyrillic);
-    u8g2.setCursor(4, 42);
-    u8g2.print("Тест 123 test");
+  u8g2.print("Exchange rate: ");
+  u8g2.print(exchange_rate);
   u8g2.sendBuffer();					
   delay(1000);
 }
