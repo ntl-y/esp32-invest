edc86a3a8f11e2352ba207e8e1f8a9a2db0ea618
ntl-y <n-danilkina@list.ru>
2024-05-21 16:30:08 +0300

    currency

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,46 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-const (
-	port = ":8999"
-)
-
 func main() {
+	port := ":8999"
+	key := "CG-Wwke7hzEzDqubwat4rf86LrN"
+	url := "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cthe-open-network&vs_currencies=usd&precision=5"
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		randomNumber := rand.Intn(100) + 1
-		response := fmt.Sprintf(`{"random_number": %d}`, randomNumber)
-		fmt.Fprint(w, response)
+
+		response := makeRequest(url, key)
+		w.Write(response)
 	})
 
 	fmt.Printf("start server at %s", port)
 	http.ListenAndServe(port, nil)
+}
+
+func makeRequest(url string, key string) []byte {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("x-cg-demo-api-key", key)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Error making request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %v", err)
+	}
+	return body
 }
